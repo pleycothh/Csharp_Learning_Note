2ef9005da293e928889ec5899d84348e78ca5f90
Ben Li <ben.li19930119@gmail.com>
2021-12-05 20:00:38 +1000

    pull request

---
+++ Testability/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testability
+{
+    public class Order
+    {
+        private int _isShipped;
+        private float _totalPrice;
+        private DateTime _orderTime;
+    }
+
+    public Order(int IsShipped, DateTime OrderTime, float TotalPrice)
+    {
+        this._isShipped = new IsShipped();
+        _taotalPrice = new TotalPrice;
+        _ordertime = OrderTime;
+
+    }
+}
---
+++ Testability/OrderProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testability
+{
+    public class OrderProcessor
+    {
+        private readonly ShippingCalculator _shippingCalculator; // pass the calculator class from outside
+
+        public OrderProcessor()
+        {
+            _shippingCalculator = new ShippingCalculator(); // init the outside ship calculator class
+        }
+
+
+        // Unit test for process
+        public void Process (Order order)
+        {
+            if (order.IsShipped) // order method return bool
+                throw new InvalidOperationException("This order is already processed.");
+
+            order.Shipment = new Shipment;
+            {
+                Cost = _shippingCalculator.CalculateShipping(order); // use method in outside calculator to get cost
+                ShippingDate = DateTime.Today.AddDays(1);
+            }
+        }
+    }
+}
