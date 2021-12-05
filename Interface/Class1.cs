a231a3ff9b46eccfeb0c808d7c337071eaa468b4
Ben Li <ben.li19930119@gmail.com>
2021-12-05 19:54:58 +1000

    add Interface

---
+++ Interface/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    static class Program1
+    {
+        static void Main(string[] args)
+        {
+            var orderProcessor = new OrderProcessor();
+            var order = new OrderedParallelQuery { DataPlaced = DateTime.Now, TotalPrice = 100f };
+            orderProcessor.Process(order);
+        }
+    }
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+/*
+ * Interface is simmler to class but fundemently different
+ * Interface do not have implementation
+ * used for loosely-coupled application
+ * 
+ */
+
+
+
+
+
+namespace Interface
+{
+
+    public interface ITaxCalculator
+    {
+        int Calculate(); 
+        //method only declear an body
+        // does not has access modfier like Public, Private
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
