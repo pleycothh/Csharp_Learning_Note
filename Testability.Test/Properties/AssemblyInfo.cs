221e1bd5099a20a05dbdc8075902d265bb70e50c
Ben Li <ben.li19930119@gmail.com>
2021-12-05 16:52:39 +1000

    Add testability

---
+++ Testability.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testability.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Griffith University")]
+[assembly: AssemblyProduct("Testability.Test")]
+[assembly: AssemblyCopyright("Copyright © Griffith University 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2e453225-d2de-48e2-9a7a-ba73c92d4b41")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Testability/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Testability
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var orderProcessor = new OrderProcessor(); // create new order processor class
+
+            var order = new OrderedParallelQuery { DatePlaced = DateTime.Now, TotalPrice = 100f };
+
+            orderProcessor.Process(order);
+        }
+    }
+}
