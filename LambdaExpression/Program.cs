0c56a033a9d53393299a5753b1bd55d36a0e92a9
Ben Li <ben.li19930119@gmail.com>
2022-01-02 03:31:53 +1000

    add Lambda

---
+++ LambdaExpression/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+using System.Collections.Generic;
+//Func<int, int> sq = Squre;
+/*
+int factor = 5; 
+Func<int,int> multiplire = n => n * factor;
+Console.WriteLine(multiplire(5));
+Console.ReadLine();
+*/
+
+// second example:
+
+namespace LambdaExpression
+{
+    public class Book
+    {
+        public int Price { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class BookRepository: Book
+    {
+
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book() {Title = "asd", Price = 1 },
+                new Book() {Title = "asd", Price = 2 },
+                new Book() {Title = "asd", Price = 3 }
+            };
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+            var cheapbooks = books.FindAll(book => book.Price > 1);
+            // findall function, loop through each element in List, check true or fules and return if ture
+
+            Console.WriteLine(cheapbooks.Count);
+            //books.FindAll;
+        }
+    }
+}
